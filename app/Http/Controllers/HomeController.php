73ef81964330d1cec2e2789b8cf91370571a81d3
kimura121212 <ykimuranoi@gmail.com>
2019-10-19 14:36:55 +0900

    スワイプ画面にユーザー一覧表示

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $users = User::all();
+
+        return view('home', compact('users'));
     }
 }
